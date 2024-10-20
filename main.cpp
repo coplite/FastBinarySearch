7a9140e7564f16752eb441bab781bf02571f6b15
coplite <96002960+coplite@users.noreply.github.com>
2024-10-20 00:11:37 -0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+
+extern "C" int BinarySearch(int*, int, int);
+
+int main()
+{
+    int array[] = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};
+    std::sort(array, array + sizeof(array)/sizeof(int)); // sorting stuff i might dont not need
+    std::cout << "sizeof array: " << sizeof(array)/sizeof(int) << "\n";
+    int rest = BinarySearch(array, (sizeof(array)/sizeof(int)), 89);
+    if(rest != -1)
+    {
+        std::cout << "index of given element: " << rest << "\n";
+    }
+    else
+    {
+        std::cout << "Element not found!!!\n";
+    }
+    return 0;
+}
