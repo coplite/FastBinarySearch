c68fdcfa0e2d519f2b63056c7ef0165acd578d10
coplite <96002960+coplite@users.noreply.github.com>
2024-10-20 00:19:43 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -8,7 +8,7 @@ int main()
     int array[] = {1, 2, 3, 5, 8, 13, 21, 34, 55, 89};                     // any rando sorted array  
     std::sort(array, array + sizeof(array)/sizeof(int));                   // sorting stuff i might dont not need
     std::cout << "sizeof array: " << sizeof(array)/sizeof(int) << "\n";
-    int rest = BinarySearch(array, (sizeof(array)/sizeof(int)), 89);       //BinarySearch(int* array, int size, int element);
+    int rest = BinarySearch(array, (sizeof(array)/sizeof(int)), 89);       // BinarySearch(int* array, int size, int element);
     if(rest != -1)
     {
         std::cout << "index of given element: " << rest << "\n";
