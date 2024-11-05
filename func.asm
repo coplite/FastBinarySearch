81848f1f714c4a49533c5688bb640faa9d024c50
coplite <96002960+coplite@users.noreply.github.com>
2024-11-04 23:16:56 -0800

    Update func.asm

--- func.asm
+++ func.asm
@@ -6,11 +6,11 @@ BinarySearch:
     xor r10, r10                      ; low
 
 _loop:
-    xor rax, rax                      ; mid && we zero this register because if we dont rax will 
+    xor rax, rax                      ; mid && we zero this register because if we dont rax will        ; we can move this back into BinarySearch
                                       ; keep adding up after iterations messing with the calculations
     cmp r10, rsi
     jg _error                         ; if(low > high) then exit
-    add rax, rsi                      ; mid + high
+    add rax, rsi                      ; mid + high                                                      ; we can replace add with mov
     add rax, r10                      ; mid + low
     shr rax, 1                        ; mid // 2   div rax, 2
     cmp DWORD [rdi+(rax*4)],  edx     ; i dont like you
